aa9c2b77bb92a916418f18716d8c3a25166c80cf
Lundberg <ilundberg@princeton.edu>
2019-04-14 08:58:51 -0500

    Housing assistance PAA 2019 presentation

--- fragile_families_PAA2019/code/weight_figure_code.R
+++ fragile_families_PAA2019/code/weight_figure_code.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(haven)
 
-d <- read_dta("/Users/iandl/Downloads/FF_wave1_2019.dta")
+#d <- read_dta("/Users/iandl/Downloads/FF_wave1_2019.dta")
+d <- read_dta("/Users/iandl/Downloads/FF_wave6_2019.dta")
 forplot <- d %>%
-  filter(!is.na(m1natwt)) %>%
-  arrange(-m1natwt) %>%
-  mutate(prop_weight = cumsum(m1natwt) / sum(m1natwt),
+  filter(!is.na(k6natwt)) %>%
+  arrange(-k6natwt) %>%
+  mutate(prop_weight = cumsum(k6natwt) / sum(k6natwt),
          prop_sample = (1:n()) / n()) %>%
   select(prop_weight, prop_sample)
 
@@ -94,6 +95,32 @@ forplot %>%
   theme(axis.text = element_text(face = "bold"),
         axis.title = element_blank()) +
   ggsave("/Users/iandl/Dropbox/slides_for_FF_workshop/figures/weight_issue_3.pdf",
+         height = 1.8, width = 1.9)
+
+forplot %>%
+  ggplot(aes(x = prop_sample, y = prop_weight)) +
+  annotate(geom = "segment", x = .25, xend = .25, y = 0, yend = p25$prop_weight,
+           color = "seagreen4", size = 1.15) +
+  annotate(geom = "segment", x = .25, xend = 0, y = p25$prop_weight, yend = p25$prop_weight,
+           color = "seagreen4", size = 1.15) +
+  annotate(geom = "segment", x = 0, xend = 1, y = 0, yend = 1, linetype = "dashed",
+           color = "darkgray") +
+  geom_line(color = "blue", size = 1.3) +
+  annotate(geom = "text", x = .65, y = .55,
+           label = "If simple random sample",
+           size = 2.5,
+           angle = 45, color = "darkgray", fontface = "bold") +
+  #xlab("\nProportion of sample") +
+  #ylab("Proportion of weight\n") +
+  scale_x_continuous(breaks = c(0,.25,1), minor_breaks = NULL,
+                     labels = function(x) format(round(x,2), digits = 2)) +
+  scale_y_continuous(breaks = c(0,p25$prop_weight,1), minor_breaks = NULL,
+                     labels = function(x) format(round(x,2), digits = 2)) +
+  theme_bw() +
+  coord_fixed() +
+  theme(axis.text = element_text(face = "bold"),
+        axis.title = element_blank()) +
+  ggsave("/Users/iandl/Downloads/weight_issue_3_y15.pdf",
          height = 1.8, width = 1.9)
 
   
